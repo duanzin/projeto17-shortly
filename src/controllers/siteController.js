6f9dd1744de5fbd14d00b8f6e85b61150ff0ace4
duanzin <lucaduan@gmail.com>
2023-03-03 12:56:44 -0300

    feat: pega url pelo id

--- src/controllers/siteController.js
+++ src/controllers/siteController.js
@@ -1,22 +1,32 @@
 import { db } from "../config/database.js";
 
 export async function getUrl(req, res) {
+  const { id } = req.params;
   try {
+    const url = await db.query(
+      `SELECT id,"shortUrl",url FROM urls WHERE id=$1`,
+      [id]
+    );
+    if (url.rows.length == 0) {
+      return res.sendStatus(404);
+    }
+    return res.status(200).send(url.rows[0]);
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 
 export async function openUrl(req, res) {
-    try {
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
+  const { url } = req.params;
+  try {
+  } catch (err) {
+    res.status(500).send(err.message);
   }
-  
-  export async function ranking(req, res) {
-    try {
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
+}
+
+export async function ranking(req, res) {
+  try {
+  } catch (err) {
+    res.status(500).send(err.message);
   }
+}

af44c79fdf29599bb4955c752020326240202b2e
duanzin <lucaduan@gmail.com>
2023-03-03 12:40:24 -0300

    feat: exclusão e renderização de urls do usuario implementada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,14 +20,47 @@ export async function shortenUrl(req, res) {
 }
 
 export async function deleteUrl(req, res) {
+  const { userId } = res.locals.session;
+  const { urlId } = req.params;
   try {
+    const url = await db.query(`SELECT * FROM urls WHERE id=$1;`, [urlId]);
+    if (url.rows.length == 0) {
+      return res.sendStatus(404);
+    }
+    if (url.rows[0].userId !== userId) {
+      return res.sendStatus(401);
+    }
+    await db.query(`DELETE FROM urls WHERE id=$1`, [urlId]);
+    return res.sendStatus(204);
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 
 export async function getUser(req, res) {
+  const { userId } = res.locals.session;
+  let totalViews = 0;
   try {
+    const user = await db.query(`SELECT id, name FROM users WHERE id=$1`, [
+      userId,
+    ]);
+    const urls = await db.query(
+      `SELECT id,"shortUrl",url,views FROM "ShortenedUrls" WHERE "userId"=$1`,
+      [userId]
+    );
+
+    for (let i = 0; i < urls.rows.length; i++) {
+      totalViews += urls.rows[i].views;
+    }
+
+    const userObj = {
+      id: user.rows[0].id,
+      name: user.rows[0].name,
+      visitCount: totalViews,
+    };
+
+    userObj.shortenedUrls = urls.rows;
+    res.status(200).send(userObj);
   } catch (err) {
     res.status(500).send(err.message);
   }

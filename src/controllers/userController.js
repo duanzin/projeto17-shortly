f9975d5eae1bb552aa30a789a8cedfcde211a736
duanzin <lucaduan@gmail.com>
2023-03-03 12:09:48 -0300

    feat: upload de urls implementado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,19 @@
 import { db } from "../config/database.js";
+import { nanoid } from "nanoid";
 
 export async function shortenUrl(req, res) {
+  const { userId } = res.locals.session;
+  const { url } = req.body;
   try {
+    const shortUrl = nanoid();
+    await db.query(
+      `INSERT INTO urls (url,"userId","shortUrl") VALUES ($1,$2,$3)`,
+      [url, userId, shortUrl]
+    );
+    const id = await db.query(`SELECT id FROM urls WHERE "shortUrl" = $1`, [
+      shortUrl,
+    ]);
+    res.status(201).send({ id: id.rows[0], shortUrl: shortUrl });
   } catch (err) {
     res.status(500).send(err.message);
   }

b1b860ff665bb11900b358ac6d4cffe84b7f7688
duanzin <lucaduan@gmail.com>
2023-03-04 11:29:16 -0300

    chore: adjust code for deploy

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,7 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
+  ssl: true,
 };
-
-if (process.env.MODE === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
